8f63d8fd1f92111e0fed42646c5c024f2b412e38
Marc Norris <shaky411@googlemail.com>
2024-06-10 10:08:14 +0100

    updates

--- app/page.js
+++ app/page.js
@@ -5,13 +5,13 @@ import mainImage from "../assets/Image-1.jpg"
 import Image from "next/image";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faInfo, faInfoCircle, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center max-w-5xl mx-auto p-4">
-      <div className="flex flex-col items-start justify-center m-10">
+      <div className="flex flex-col items-start justify-center m-6">
       <Image
       src={mainImage}
       width={300}
@@ -22,10 +22,10 @@ export default function Home() {
         <h1 className={`${chakra.className} font-bold text-4xl text-slate-600`}>Password Generator</h1>
         <span className="text-lg font-light">Create secure passwords and easily copy them to your clipboard.</span>
         </div>
-        <Link href="/info" className="border-[#FD735B] hover:bg-[#FD735B] hover:text-white border px-2 py-1 rounded-lg mt-4 duration-500">Info</Link>
+        <Link href="/info" className="border-[#FD735B] text-slate-600 hover:bg-[#FD735B] hover:text-white border px-2 py-1 rounded-lg mt-4 duration-500"><FontAwesomeIcon className="mr-1" icon={faInfoCircle} />Info</Link>
       </div>
       <Password />
-      <span className="text-sm mt-4 my-10 text-rose-400"><FontAwesomeIcon className="mr-2" icon={faTriangleExclamation} />Once copied, please save the password in a secure location such as a password manager.</span>
+      <span className="text-sm mt-4 my-10 text-rose-400"><FontAwesomeIcon className="mr-1" icon={faTriangleExclamation} />Once copied, please save the password in a secure location such as a password manager.</span>
     </main>
   );
 }
