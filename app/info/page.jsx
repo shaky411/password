ae96c35888fae29364dbdc03cc78ad37f4d100dc
Marc Norris <shaky411@googlemail.com>
2024-06-12 06:48:34 +0100

    added hardware

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -2,91 +2,150 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { faInfo, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Info() {
-    return (
-        <main className="flex flex-col items-center justify-center max-w-4xl mx-auto p-10 sm:p-4">
+  return (
+    <main className="flex flex-col items-center justify-center max-w-4xl mx-auto p-10 sm:p-4">
+      <div className="mt-10 flex flex-col items-center justify-center gap-4">
+        <h2 className="text-4xl font-light">Info</h2>
+        <p className="font-light">
+          Creating secure passwords is crucial for protecting your personal and
+          sensitive information. Here are some guidelines and tips to help you
+          create strong and secure passwords:
+        </p>
+      </div>
 
-            <div className="mt-10 flex flex-col items-center justify-center gap-4">
-            <h2 className="text-4xl font-light">Info</h2>
-            <p className="font-light">Creating secure passwords is crucial for protecting your personal and sensitive information. Here are some guidelines and tips to help you create strong and secure passwords:</p>
-            </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-7 mt-10">
+        <div className="col-span-2 sm:col-span-1 p-4 border rounded-lg bg-indigo-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2">
+          <span className="font-semibold">Length</span>
+          <li>
+            Longer is better: Aim for at least 12-16 characters. Longer
+            passwords are generally more secure.
+          </li>
+        </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-7 mt-10">
-                <div className="col-span-2 sm:col-span-1 p-4 border rounded-lg bg-indigo-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2">
-                    <span className="font-semibold">
-                        Length
-                    </span>
-                    <li>Longer is better: Aim for at least 12-16 characters. Longer passwords are generally more secure.</li>
-                </div>
+        <div className="p-4 border rounded-lg bg-green-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 col-span-2">
+          <span className="font-semibold">Complexity</span>
+          <li>
+            Mix of character types: Use a combination of uppercase letters,
+            lowercase letters, numbers, and special characters (e.g., @, #, $,
+            %, &, etc.).
+          </li>
+          <li>
+            Avoid predictable patterns: Don't use common substitutions (e.g.,
+            "P@ssw0rd"), sequences (e.g., "123456"), or repeated characters.
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-green-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 col-span-2">
-                    <span className="font-semibold">
-                        Complexity
-                    </span>
-                    <li>Mix of character types: Use a combination of uppercase letters, lowercase letters, numbers, and special characters (e.g., @, #, $, %, &, etc.).</li>
-                    <li>Avoid predictable patterns: Don't use common substitutions (e.g., "P@ssw0rd"), sequences (e.g., "123456"), or repeated characters.</li>
-                </div>
+        <div className="p-4 border rounded-lg bg-yellow-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 col-span-2">
+          <span className="font-semibold">Unpredictability</span>
+          <li>
+            Randomness is key: Use a password generator to create truly random
+            passwords.
+          </li>
+          <li>
+            Avoid dictionary words: Single dictionary words are easily cracked
+            by brute-force attacks.
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-yellow-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 col-span-2">
-                    <span className="font-semibold">
-                        Unpredictability
-                    </span>
-                    <li>Randomness is key: Use a password generator to create truly random passwords.</li>
-                    <li>Avoid dictionary words: Single dictionary words are easily cracked by brute-force attacks.</li>
-                </div>
+        <div className="p-4 border rounded-lg bg-red-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
+          <span className="font-semibold">Uniqueness</span>
+          <li>
+            Different for each account: Use a unique password for every account
+            to prevent a breach on one site from compromising others.
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-red-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
-                    <span className="font-semibold">
-                    Uniqueness
-                    </span>
-                    <li>Different for each account: Use a unique password for every account to prevent a breach on one site from compromising others.</li>
-                </div>
+        <div className="p-4 border rounded-lg bg-slate-300 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
+          <span className="font-semibold">Password Managers</span>
+          <li>
+            Use a password manager: Tools like LastPass, 1Password, or Bitwarden
+            can generate, store, and manage your passwords securely. This way,
+            you only need to remember one master password.
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-slate-300 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
-                    <span className="font-semibold">
-                    Password Managers
-                    </span>
-                    <li>Use a password manager: Tools like LastPass, 1Password, or Bitwarden can generate, store, and manage your passwords securely. This way, you only need to remember one master password.</li>
-                </div>
+        <div className="p-4 border rounded-lg bg-purple-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
+          <span className="font-semibold">Regular Updates</span>
+          <li>
+            Change passwords regularly: Especially if you suspect an account has
+            been compromised.
+          </li>
+          <li>
+            Monitor for breaches: Use services like Have I Been Pwned to check
+            if your email or password has been involved in a data breach.
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-purple-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
-                    <span className="font-semibold">
-                    Regular Updates
-                    </span>
-                    <li>Change passwords regularly: Especially if you suspect an account has been compromised.</li>
-                    <li>Monitor for breaches: Use services like Have I Been Pwned to check if your email or password has been involved in a data breach.</li>
-                </div>
+        <div className="p-4 border rounded-lg bg-pink-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
+          <span className="font-semibold">
+            Multi-Factor Authentication (MFA)
+          </span>
+          <li>
+            Use a password manager: Tools like LastPass, 1Password, or Bitwarden
+            can generate, store, and manage your passwords securely. This way,
+            you only need to remember one master password.
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-pink-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
-                    <span className="font-semibold">
-                    Multi-Factor Authentication (MFA)
-                    </span>
-                    <li>Use a password manager: Tools like LastPass, 1Password, or Bitwarden can generate, store, and manage your passwords securely. This way, you only need to remember one master password.</li>
-                </div>
+        <div className="p-4 border rounded-lg bg-orange-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-3 col-span-2">
+          <span className="font-semibold">Examples of Strong Passwords</span>
+          <li>
+            Randomly generated: <strong>hT7&3z@Q5!mL0xW</strong>
+          </li>
+          <li>
+            Passphrases: Create a long and unique phrase using random words or
+            an easy-to-remember sentence with some complexity added.
+          </li>
+          <li>
+            Example: <strong>CorrectHorseBatteryStaple!92</strong>
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-orange-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-3 col-span-2">
-                    <span className="font-semibold">
-                    Examples of Strong Passwords
-                    </span>
-                    <li>Randomly generated: <strong>hT7&3z@Q5!mL0xW</strong></li>
-                    <li>Passphrases: Create a long and unique phrase using random words or an easy-to-remember sentence with some complexity added.</li>
-                    <li>Example: <strong>CorrectHorseBatteryStaple!92</strong></li>
-                </div>
+        <div className="p-4 border rounded-lg bg-lime-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-1 col-span-2">
+          <span className="font-semibold">Bad Practices to Avoid</span>
+          <li>
+            Personal Information: Avoid using easily guessable information like
+            your name, birthday, or pet's name.
+          </li>
+          <li>
+            Common Passwords: Do not use passwords like "password," "123456," or
+            "qwerty."
+          </li>
+        </div>
 
-                <div className="p-4 border rounded-lg bg-lime-500 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-3 col-span-2">
-                    <span className="font-semibold">
-                    Bad Practices to Avoid
-                    </span>
-                    <li>Personal Information: Avoid using easily guessable information like your name, birthday, or pet's name.</li>
-                    <li>Common Passwords: Do not use passwords like "password," "123456," or "qwerty."</li>
-                </div>
-            </div>
+        <div className="p-4 border rounded-lg bg-slate-200 hover:shadow-lg duration-300 bg-opacity-40 flex flex-col gap-2 sm:col-span-2 col-span-2">
+          <span className="font-semibold">Hardware</span>
+          <p>
+            Using a two-factor authentication (2FA) security key greatly
+            enhances online security by providing robust protection against
+            phishing and password theft. Unlike SMS or app-based codes, security
+            keys require physical possession, making unauthorized access
+            significantly harder. They are easy to use, work offline, and are
+            compatible with a wide range of devices and platforms. This added
+            layer of security ensures that even if your password is compromised,
+            your accounts remain secure, offering peace of mind and compliance
+            with stringent security standards.
+          </p>
+          <Link
+            className="text-blue-500 hover:opacity-40 duration-300 mr-auto my-2"
+            href="https://www.amazon.co.uk/Yubico-Two-factor-authentication-security-certified/dp/B08DHL1YDL/ref=sr_1_5?crid=7AN8DI868Y5Y&amp;dib=eyJ2IjoiMSJ9.6mEnrMWeKBC9SREHb4zIrcm-tlYt5XW6TkAfcc1UGMdVLqlY1hDPhcp_PM6UeXg6ebRS4RQGe7pvc6SZ2CMJ5mepW-1IwCNYketmOfOsCbZtPVc-HKD2wcuA4sUdAK3g0U0fJrqB5MDYOtax_ACMkstX-AwmiCShGmT26WWJrZZKHGHBTIUw5A4BBD9NauYISHX4-sNyW6DcOJ5_lKiC1VagVbTUR5VtW_DmqxnvTUo.QFwAUoPBz5QNhx2gO_mc04yoBZkf0fgsQzCWDHyxLqk&amp;dib_tag=se&amp;keywords=password+key&amp;qid=1718119563&amp;sprefix=password+key%252Caps%252C86&amp;sr=8-5&_encoding=UTF8&tag=graygecko-21&linkCode=ur2&linkId=e1a1b0ffd94ffca2254650b64867cd51&camp=1634&creative=6738"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            more info
+          </Link>
+        </div>
+      </div>
 
-            <Link className="my-10 mr-auto bg-blue-500 hover:opacity-50 duration-400 px-4 py-2 text-white rounded" href="/">&larr; Back</Link>
-
-            
-        </main>
-    )
+      <Link
+        className="my-10 mr-auto bg-blue-500 hover:opacity-50 duration-400 px-4 py-2 text-white rounded"
+        href="/"
+      >
+        &larr; Back
+      </Link>
+    </main>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -6,12 +6,14 @@ import Image from "next/image";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faCartShopping,
   faInfo,
   faInfoCircle,
   faTriangleExclamation,
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import Managers from "./Components/managers";
+import securitykey from "../public/security_key.jpg";
 
 export default function Home() {
   return (
@@ -44,7 +46,7 @@ export default function Home() {
           Once copied, please save the password in a secure location such as a
           password manager.
         </span>
-        <p className="font-light mb-10">
+        <p className="font-light">
           In the digital age, passwords are fundamental to safeguarding your
           online presence and assets. Implementing strong password practices is
           essential for maintaining the integrity, confidentiality, and security
@@ -52,13 +54,46 @@ export default function Home() {
         </p>
       </div>
 
-<div className="mr-auto">
-<Managers />
-</div>
-      
-      <span className="mr-auto mt-10 text-tiny text-slate-400">Site created by M.Norris ©2024</span>
-      
-      
+      <span className="mr-auto my-4">My Hardware Recommendation</span>
+
+      <div className="flex flex-col sm:flex-row justify-center items-center mr-auto gap-10 border p-6 rounded-lg shadow">
+        <Image
+          src={securitykey}
+          width={150}
+          height={150}
+          alt="security key image"
+        />
+
+        <p className="font-light">
+          Using a two-factor authentication (2FA) security key greatly enhances
+          online security by providing robust protection against phishing and
+          password theft. Unlike SMS or app-based codes, security keys require
+          physical possession, making unauthorized access significantly harder.
+          They are easy to use, work offline, and are compatible with a wide
+          range of devices and platforms. This added layer of security ensures
+          that even if your password is compromised, your accounts remain
+          secure, offering peace of mind and compliance with stringent security
+          standards.
+        </p>
+      </div>
+
+      <Link
+        href="https://www.amazon.co.uk/Yubico-Two-factor-authentication-security-certified/dp/B08DHL1YDL/ref=sr_1_5?crid=7AN8DI868Y5Y&amp;dib=eyJ2IjoiMSJ9.6mEnrMWeKBC9SREHb4zIrcm-tlYt5XW6TkAfcc1UGMdVLqlY1hDPhcp_PM6UeXg6ebRS4RQGe7pvc6SZ2CMJ5mepW-1IwCNYketmOfOsCbZtPVc-HKD2wcuA4sUdAK3g0U0fJrqB5MDYOtax_ACMkstX-AwmiCShGmT26WWJrZZKHGHBTIUw5A4BBD9NauYISHX4-sNyW6DcOJ5_lKiC1VagVbTUR5VtW_DmqxnvTUo.QFwAUoPBz5QNhx2gO_mc04yoBZkf0fgsQzCWDHyxLqk&amp;dib_tag=se&amp;keywords=password+key&amp;qid=1718119563&amp;sprefix=password+key%252Caps%252C86&amp;sr=8-5&_encoding=UTF8&tag=graygecko-21&linkCode=ur2&linkId=e1a1b0ffd94ffca2254650b64867cd51&camp=1634&creative=6738"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        className="text-blue-500 hover:opacity-40 duration-300 ml-auto my-4"
+      >
+        <FontAwesomeIcon className="mr-1" icon={faCartShopping}/>
+        Purchase on Amazon UK
+      </Link>
+
+      <div className="mr-auto">
+        <Managers />
+      </div>
+
+      <span className="mr-auto mt-10 text-tiny text-slate-400">
+        Site created by M.Norris ©2024
+      </span>
     </main>
   );
 }
