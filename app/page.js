9e1cc1dd4a19ada266849244127bac18db9714f8
Marc Norris <shaky411@googlemail.com>
2024-06-13 14:13:00 +0100

    updated footer

--- app/page.js
+++ app/page.js
@@ -103,15 +103,15 @@ export default function Home() {
       
 
       <div className="mt-10 mr-auto">
-        <span className="mr-2 text-tiny text-slate-400">Site created by</span>
-        <Link
+        <span className="mr-2 text-tiny text-slate-400">Site created by <strong>GrayGecko</strong> ©2024</span>
+        {/* <Link
           className="bg-slate-100 hover:opacity-40 duration-300 px-4 py-2 mr-auto mt-4 rounded-full border text-sm font-semibold text-blue-500"
           href="https://www.graygecko.co.uk"
           target="_blank"
           rel="noopener noreferrer nofollow"
         >
           GrayGecko Web Design
-        </Link>
+        </Link> */}
       </div>
     </main>
   );
