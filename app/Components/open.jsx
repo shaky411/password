b75a0fb57d25f43931d2e782ffef96d0d1156065
Marc Norris <shaky411@googlemail.com>
2024-06-14 08:51:43 +0100

    updated github area

--- app/Components/open.jsx
+++ app/Components/open.jsx
@@ -7,10 +7,10 @@ import githubImage from "../../assets/github.png";
 export default function Open() {
   return (
     <main>
-      <div className="flex bg-slate-100 p-10 rounded-lg justify-center items-center border gap-5 shadow">
+      <div className="flex flex-col sm:flex-row bg-slate-100 p-10 rounded-lg justify-center items-center border gap-5 shadow">
         <Image src={githubImage} width={200} height={200} alt="github logo" />
 
-        <div className="w-1/2 flex flex-col gap-4">
+        <div className="sm:w-1/2 flex flex-col gap-4">
           <p>
             I believe in transparency when working with sensitive information
             which is why I have decided to make this password generator, <strong>open
@@ -18,7 +18,7 @@ export default function Open() {
           </p>
 
           <Link
-            className="bg-indigo-500 hover:opacity-40 duration-300 w-fit px-4 py-2 rounded-full text-white mx-auto"
+            className="bg-indigo-500 hover:opacity-40 duration-300 w-full text-center sm:w-fit px-4 py-2 rounded-full text-white mx-auto"
             href="https://github.com/shaky411/password.git"
             target="_blank"
             rel="noopener noreferrer nofollow"
