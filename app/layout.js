97b582463e6b8e04e63101c5a444ab7a148a2d89
Marc Norris <shaky411@googlemail.com>
2024-06-14 10:04:47 +0100

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ogImage from "./opengraph-image.png"
+import { Analytics } from "@vercel/analytics/react";
+// import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -48,7 +50,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {/* <SpeedInsights /> */}
+        <Analytics />
+        </body>
     </html>
   );
 }
