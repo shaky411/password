341aadc08e53e4e52ac0bca1105fa097d152ced7
Marc Norris <shaky411@googlemail.com>
2024-10-25 12:52:06 +0100

    added google adsense script

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 // import { SpeedInsights } from "@vercel/speed-insights/next"
 import Footer from "./Components/footer";
 import Navbar from "./Components/navbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -52,6 +53,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <Script
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          data-ad-client="ca-pub-6386565516888254"
+          async
+        />
+      </head>
       {/* <Navbar /> */}
       <body className={inter.className}>
         <Navbar />
